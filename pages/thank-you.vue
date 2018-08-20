1a4f5bb9ddcc5e659cd542a287d299da2ac4f089
resoul <resoul@bk.ru>
2018-08-20 17:17:24 +0300

    created thank you page

---
+++ pages/thank-you.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <div class="hnn">
+            <div class="hnn__yellow_hero">
+                <h2 class="h1">Your application has been submitted. Thank you!</h2>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        data : function (){}
+    }
+</script>
