386e232c3bab4762b5ad723e6a59c95e327bddeb
resoul <resoul@bk.ru>
2018-08-21 10:12:01 +0300

    created thank you page

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -12,19 +12,4 @@
     export default {
         data : function (){}
     }
-
-    function gtag_report_conversion(url) {
-        var callback = function () {
-            if (typeof(url) != 'undefined') {
-                window.location = url;
-            }
-        };
-        gtag('event', 'conversion', {
-            'send_to': 'AW-792986929/La1TCOKtsocBELGKkPoC',
-            'event_callback': callback
-        });
-        return false;
-    }
-
-    gtag_report_conversion();
 </script>
